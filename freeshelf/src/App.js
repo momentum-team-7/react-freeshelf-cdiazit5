11c05b64d14b0bd0f9b53eb21361d522e75bdb08
cdiazit5 <cdiazit5@gmail.com>
2021-03-21 22:57:38 -0400

    button for showing more or less is weird but working

--- freeshelf/src/App.js
+++ freeshelf/src/App.js
@@ -5,26 +5,46 @@ import ReactDOM from 'react-dom'
 
 
 const App = () => {
-  const [books] = useState(data)
+  const [books, setBook] = useState(data)
   // set the value of books to the array of objects
   return (
     <div className='react-root'>
-      <h1>Freeshelf App</h1>
+      <h1 className='header'>Freeshelf App</h1>
       {books.map((book) => (
-        <Book title={book.title} author={book.author} description={book.shortDescription} image={book.coverImageUrl} />
+        <Book 
+          title={book.title} 
+          author={book.author} 
+          description={book.shortDescription} 
+          image={book.coverImageUrl}
+          url={book.url} 
+          publisher={book.publisher} 
+          publicationDate={book.publicationDate} 
+          detailedDescription={book.detailedDescription}
+        />
       ))}
     </div>
   )
 }
 
 const Book = (props) => {
-  const { title, author, description, image } = props
+  const { title, author, description, image, url, publisher, publicationDate, detailedDescription } = props
+  const [isLess, setisMore] = useState(false)
+  const onClick = () => setisMore(!isLess)
   return (
     <div className='book-info'>
+      <div className='book-text'>
+        <h3>{title}</h3>
+        <h4>{author}</h4>
+        <p>{description}</p>
+        <div style={isLess? {} : { display:'none' }}>
+          <p>URL: {url} </p>
+          <p>Publisher: {publisher}</p>
+          <p>Publication Date: {publicationDate}</p>
+          <p>Detailed Desciption: {detailedDescription} </p>
+        </div>
+      </div>
+      <button onClick={onClick}>{isLess ? "See less" : "See More"}</button>
       <img className='book-image' src={image} alt='image' />
-      <h3>{title}</h3>
-      <h4>{author}</h4>
-      <p>{description}</p>
     </div>
   )
 }
